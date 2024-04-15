461e536074b7148ffade607a9de413ac372711c3
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-04-15 17:10:45 +0200

    New Project

---
+++ Main.py
@@ -0,0 +1,16 @@
+class Tools:
+    def __init__(self, name):
+        self.name = name
+
+    def Use(self):
+        print(f"You are using {self.name}")
+
+    @staticmethod
+    def take_in_hand():
+        print("You picked up the instrument")
+
+if __name__ == "__main__":
+    shovel = Tools("shovel")
+
+    Tools.take_in_hand()
+    shovel.Use() #Tools.Use() Error
